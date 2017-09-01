cfdcd6a2078be15215adc2ed97b2fad00331e15e
PC-20170317VGPA\Wilson <845023508@qq.com>
2017-09-01 12:14:18 +0800

    no message

--- src/main/java/per/wilson/validation/validator/WorkerValidator.java
+++ src/main/java/per/wilson/validation/validator/WorkerValidator.java
@@ -15,12 +15,32 @@ public class WorkerValidator implements ConstraintValidator<WorkerAnnotation,Str
     private WorkerEnum allEnum;
 
     @Override
+    //验证前获取WorkerAnnotation注解中的方法值并根据需要进行初始化
     public void initialize(WorkerAnnotation constraintAnnotation) {
+//        String msg = constraintAnnotation.message();
         allEnum = constraintAnnotation.target();
     }
 
+    /**
+     *
+     * @param value 需要校验的对象，该处验证的类型是字符串，当然也可以自定义
+     * @param context 约束验证上下文
+     *                disableDefaultConstraintViolation():用于使默认ConstraintViolation失效是的能够设置不同的违反信息或生成一个基于不同属性的
+     *                ConstraintViolation
+     *                getDefaultConstraintMessageTemplate():获取默认的约束信息模板
+     *                buildConstraintViolationWithTemplate(String messageTemplate):新建一个ConstraintViolation与信息模板,需在disableDefaultConstraintViolation()
+     *                      后调用
+     * @return 校验通过返回true,否则返回false
+     */
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        /*if(StringUtils.isEmpty(value)){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("工作者参数不能为null")
+//                    .addPropertyNode("status")   该方法用于针对校验类中的属性,如value类型是一个User对象，则为user.status属性添加非空约束,失败信息为工作者参数不能为null
+                    .addConstraintViolation();
+            return false;
+        }*/
         return allEnum.check(value);
     }
 }
